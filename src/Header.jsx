bc84c82233813f34baea2ca21c7e7dd3c66c2011
Yonatan Ben Ami <yba6081@gmail.com>
2023-11-14 12:13:05 +0200

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+  return (<>
+    <div>
+        <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Managers</li>
+            <li>Mentors</li>
+            <li>Participants</li>
+        </ul>
+    </div>
+  
+  </>
+  )
+}
