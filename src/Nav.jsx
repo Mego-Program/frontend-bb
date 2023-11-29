f0648024cd962cade001b772caff7f909f7b3610
Netanel Yaacobi <netanely3@gmail.com>
2023-11-29 12:02:24 +0200

    Routing without refreshing the entire page

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,10 +1,9 @@
-import React, { useState } from 'react'
-import './App.css'
-import BurgerButton from './BurgerButton'
-
+import React, { useState } from "react";
+import "./App.css";
+import BurgerButton from "./BurgerButton";
+import { Link } from "react-router-dom";
 
 export default function Nav() {
-
   const [activeLink, setActiveLink] = useState(null);
 
   const handleLinkClick = (linkId) => {
@@ -14,36 +13,82 @@ export default function Nav() {
 
   // פונקציה שמחזירה את ה-className בהתאם לסטייט
   const getLinkClassName = (linkId) => {
-    return linkId === activeLink ? 'color-yellow' : 'hover:color-yellow';
+    return linkId === activeLink ? "color-yellow" : "hover:color-yellow";
   };
-  return (<>
-    <header className='sticky top-0  w-full flex justify-center z-[3]'>
-      <div className=' flex flex-row items-center justify-center py-6 space-x-6 w-4/5'>
+  return (
+    <>
+      <header className="sticky top-0  w-full flex justify-center z-[3]">
+        <div className=" flex flex-row items-center justify-center py-6 space-x-6 w-4/5">
+          <div className="w-3/4 hidden lg:flex justify-end">
+            <ul className="flex text-right space-x-6">
+              <li className="hover:color-yellow">
+                <Link
+                  to="/home"
+                  onClick={() => handleLinkClick(1)}
+                  className={getLinkClassName(1)}
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="hover:color-yellow">
+                <Link
+                  to="/about"
+                  onClick={() => handleLinkClick(2)}
+                  className={getLinkClassName(2)}
+                >
+                  About Us
+                </Link>
+              </li>
+              <li className="hover:color-yellow">
+                <Link
+                  to="/managers"
+                  onClick={() => handleLinkClick(3)}
+                  className={getLinkClassName(3)}
+                >
+                  Managers
+                </Link>
+              </li>
+              <li className="hover:color-yellow">
+                <Link
+                  to="/Mentors"
+                  onClick={() => handleLinkClick(4)}
+                  className={getLinkClassName(4)}
+                >
+                  Mentors
+                </Link>
+              </li>
+              <li className="hover:color-yellow">
+                <Link
+                  to="/Participants"
+                  onClick={() => handleLinkClick(5)}
+                  className={getLinkClassName(5)}
+                >
+                  Participants
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-        <div className='w-3/4 hidden lg:flex justify-end'>
-          <ul className='flex text-right space-x-6'>
-            <li className='hover:color-yellow'><a href="/home" onClick={() => handleLinkClick(1)} className={getLinkClassName(1)}>Home</a></li>
-            <li className='hover:color-yellow'><a href="/about" onClick={() => handleLinkClick(2)} className={getLinkClassName(2)}>About Us</a></li>
-            <li className='hover:color-yellow'><a href="/managers" onClick={() => handleLinkClick(3)} className={getLinkClassName(3)}>Managers</a></li>
-            <li className='hover:color-yellow'><a href="/Mentors" onClick={() => handleLinkClick(4)} className={getLinkClassName(4)}>Mentors</a></li>
-            <li className='hover:color-yellow'><a href="/Participants" onClick={() => handleLinkClick(5)} className={getLinkClassName(5)}>Participants</a></li>
-          </ul>
+          <div className="w-1/4 flex justify-end items-end">
+            <ul className="flex text-right space-x-6">
+              <li className=" w-32 hover:color-yellow items-end">
+                <a
+                  href="#SignIn"
+                  onClick={() => handleLinkClick(6)}
+                  className={getLinkClassName(6)}
+                >
+                  Sign In
+                </a>
+              </li>
+              <li className="color-yellow b-yellow rounded-md hover:text-neutral-50">
+                <button className="btn "> Sign Up</button>
+              </li>
+            </ul>
+          </div>
         </div>
 
-        <div className='w-1/4 flex justify-end items-end'>
-          <ul className='flex text-right space-x-6'>
-            <li className=' w-32 hover:color-yellow items-end'><a href="#SignIn" onClick={() => handleLinkClick(6)} className={getLinkClassName(6)}>Sign In</a></li>
-            <li className='color-yellow b-yellow rounded-md hover:text-neutral-50'>
-              <button className='btn '> Sign Up</button>
-            </li>
-          </ul>
-        </div>
-
-      </div>
-
-      <BurgerButton />
-    </header>
-
-  </>
-  )
+        <BurgerButton />
+      </header>
+    </>
+  );
 }
