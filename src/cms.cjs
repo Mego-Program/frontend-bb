323792ab40bb90130101c0efbd7ffc0fa36c5830
ydsha <ydshamr@gmail.com>
2023-11-30 17:44:37 +0200

    transferred function from cms.cjs to cms-to-blog.js

--- src/cms.cjs
+++ src/cms.cjs
@@ -158,56 +158,6 @@ async function getCustomerReviews() {
 
 // getCustomerReviews();
 
-    // **************Internal Blog.************************** //
-async function getInternalBlog(fields = ['title', 'content', 'seo_tag']) {
-    try {
-        const items = await client.items.all({
-            filter: {
-                type: 'internal_blog',
-            },
-            fields: fields
-        });
-
-        const internalBlog = items.map(item => {
-            let result = {};
-            fields.forEach(field => {
-                result[field] = item[field];
-            });
-            return result;
-        });
-
-        console.log('Internal Blog:', internalBlog);
-        return internalBlog;
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
-// getInternalBlog(['title', 'content']); 
-
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// Previous function - gets all the fields with their data.
-// async function getInternalBlog() {
-//     try {
-//         const items = await client.items.all({
-//             filter: {
-//                 type: 'internal_blog',
-//             }
-//         });
-
-//         const internalBlog = items.map(item => ({
-//             title: item.title,
-//             content: item.content,
-//             seo_tag: item.seoTag
-//         }));
-
-//         console.log('Internal Blog:', internalBlog);
-//         return internalBlog;
-//     } catch (error) {
-//         console.error('Error:', error);
-//     }
-// }
-// getInternalBlog();
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ 
 
 
