ce5bd059ade7a85ac637d54f269c88931865caed
Yonatan Ben Ami <yba6081@gmail.com>
2023-12-04 15:01:26 +0200

    fixed

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import EmployeeCard from './EmployeeCard';
 
-function Manager() {
+
+export default function Employees() {
+
     return (<>
         <div className='flex flex-wrap w-full justify-center gap-5'>
             <EmployeeCard Name='Emma' Role='Web Developer' srcImg='img-3.png' />
@@ -16,48 +18,6 @@ function Manager() {
             <EmployeeCard Name='Emma' Role='Web Developer' srcImg='img-3.png' />
         </div>
 
-
     </>);
 }
 
-function Participants() {
-    return (<>
-        <div className="flex flex-wrap w-full justify-center  ">
-            <EmployeeCard Name='Emma' Role='Web Developer' srcImg='img-3.png' />
-            <EmployeeCard Name='Emma' Role='Web Developer' srcImg='img-3.png' />
-            <EmployeeCard Name='Emma' Role='Web Developer' srcImg='img-3.png' />
-            <EmployeeCard Name='Emma' Role='Web Developer' srcImg='img-3.png' />
-        </div>
-
-    </>);
-}
-
-function Mentors() {
-    return (<>
-
-        <EmployeeCard Name='Emma' Role='Web Developer' srcImg='img-3.png' />
-        <EmployeeCard Name='Emma' Role='Web Developer' srcImg='img-3.png' />
-    </>);
-}
-
-function Employees() {
-    const [currentComponent, setCurrentComponent] = useState(<Manager />);
-
-    const showComponent = (component) => {
-        setCurrentComponent(component);
-    };
-
-    return (
-        <div>
-            <button onClick={() => showComponent(<Manager />)}>מנהלים</button>
-            <button onClick={() => showComponent(<Participants />)}>משתתפים</button>
-            <button onClick={() => showComponent(<Mentors />)}>מורים</button>
-
-            <div>
-                {currentComponent}
-            </div>
-        </div>
-    );
-}
-
-export default Employees;
