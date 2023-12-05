001b51b33fc8482b9726ddd035e18ab98c995447
ydsha <ydshamr@gmail.com>
2023-12-05 15:40:41 +0200

    tryed to insert functions to send and save data in DatoCMS

--- datoApiClients.js
+++ datoApiClients.js
@@ -1,25 +1,25 @@
-const API_TOKEN = 'b3d15d163318321dd591d7733a32ee';
-const ENDPOINT = 'https://mego-program.admin.datocms.com/editor/item_types/fqN6Cg0rRGmc1MlsmsAeyg/items';
+// const API_TOKEN = 'b3d15d163318321dd591d7733a32ee';
+// const ENDPOINT = 'https://mego-program.admin.datocms.com/editor/item_types/fqN6Cg0rRGmc1MlsmsAeyg/items';
 
-async function createContact(contactData) {
-    const response = await fetch(ENDPOINT, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${API_TOKEN}`,
-        },
-        body: JSON.stringify({
-            data: {
-                type: 'contact', // Replace 'contact' with the actual model type identifier in DatoCMS
-                attributes: contactData,
-            },
-        }),
-    });
+// async function createContact(contactData) {
+//     const response = await fetch(ENDPOINT, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Authorization': `Bearer ${API_TOKEN}`,
+//         },
+//         body: JSON.stringify({
+//             data: {
+//                 type: 'contact', // Replace 'contact' with the actual model type identifier in DatoCMS
+//                 attributes: contactData,
+//             },
+//         }),
+//     });
 
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return await response.json();
-}
+//     if (!response.ok) {
+//         throw new Error(`HTTP error! status: ${response.status}`);
+//     }
+//     return await response.json();
+// }
 
-export { createContact };
+// export { createContact };
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import YellowButton from "./Yellowutton";
 // Added by Yakov:
-import { datoApiClient } from "./datoApiClient"; // Make sure to import the API client
+// import { datoApiClient } from "../../datoApiClients"; // Make sure to import the API client
 // The end.
 
 
@@ -22,22 +22,22 @@ function ContactForm() {
     };
 
     // Changed by Yakov:// Function to handle form submission
-    // const handleSubmit = (event) => {
-    //     event.preventDefault();
-    //     // You can perform additional actions with the form data here
-    //     console.log("Form Data:", formData);
-    // };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // You can perform additional actions with the form data here
+        console.log("Form Data:", formData);
+    };
 
     // Added by Yakov:
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            const response = await datoApiClient.createContact(formData);
-            console.log("Data saved to DatoCMS", response);
-        } catch (error) {
-            console.error("Error saving data to DatoCMS", error);
-        }
-    };
+    // const handleSubmit = async (event) => {
+    //     event.preventDefault();
+    //     try {
+    //         const response = await datoApiClient.createContact(formData);
+    //         console.log("Data saved to DatoCMS", response);
+    //     } catch (error) {
+    //         console.error("Error saving data to DatoCMS", error);
+    //     }
+    // };
     // The end.
 
     return (
