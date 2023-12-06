23d99300c7842a0dfa01c6f7d3603b7f2f6a410c
JoshRottenberg <yrott02@gmail.com>
2023-12-06 12:36:16 +0200

    directions

--- src/components/Directions.jsx
+++ src/components/Directions.jsx
@@ -8,14 +8,8 @@ function Directions() {
                 <span className="text-amber-400 "> Directions</span>
             </div>
         </div>
-        <div className='w-full h-[300px] flex justify-center rounded-[30px]'
+        <div className='w-full h-[50px] flex justify-center rounded-[30px]'
             style={{ backgroundImage: "url('map 1.png')", }}></div>
-        {/* <img className="w-[1566px] h-[548px] rounded-[30px]" src="https://via.placeholder.com/1566x548" /> */}
-        {/* <div className="w-full h-[300px] flex justify-center bg-slate-900 rounded-[30px]">
-
-
-        </div> */}
-
     </>)
 }
 export default Directions;
