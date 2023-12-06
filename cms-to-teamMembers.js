ef4f315f0b3526f1db36f3b77b4a81da215e4c4c
ydsha <ydshamr@gmail.com>
2023-12-06 23:31:11 +0200

    fixing integration between Datocms to the page

--- cms-to-teamMembers.js
+++ cms-to-teamMembers.js
@@ -2,25 +2,22 @@ import { SiteClient } from 'datocms-client';
 
 const client = new SiteClient('ec1e1d0e08445b13ea2d78bf467b27');
 
-// Team Members.
-async function getTeamMembers() {
+
+export async function getTeamMembers() {
     try {
         const items = await client.items.all({
             filter: {
-                type: 'team_member',
-                fields: {
-
-                }
+                type: 'member',
             }
         });
 
         const teamMembers = items.map(item => ({
             picture: item.picture,
             name: item.name,
-            jobTitle: item.jobTitle,
+            jtitle: item.jtitle,
             department: item.department,
-            short_description: item.shortDescription,
-            full_description: item.fullDescription
+            sdescription: item.sdescription,
+            fdescription: item.fdescription
         }));
 
         console.log('Team Members:', teamMembers);
@@ -30,102 +27,38 @@ async function getTeamMembers() {
     }
 }
 
-// 
-async function getManagers() {
-    try {
-        const items = await client.items.all({
-            filter: {
-                type: 'team_member',
-                fields: {
-                    department: {
-                        eq: 'Managers'
-                    }
-                }
-            }
-        });
+// async function getManagers() {
+//     try {
+//         const items = await client.items.all({
+//             filter: {
+//                 type: 'team_member',
+//                 fields: {
+//                     department: {
+//                         eq: 'Managers'
+//                     }
+//                 }
+//             }
+//         });
 
-        const teamMembers = items.map(item => ({
-            picture: item.picture,
-            name: item.name,
-            jobTitle: item.jobTitle,
-            department: item.department,
-            short_description: item.shortDescription,
-            full_description: item.fullDescription
-        }));
+//         const teamMembers = items.map(item => ({
+//             picture: item.picture,
+//             name: item.name,
+//             jobTitle: item.jobTitle,
+//             department: item.department,
+//             short_description: item.shortDescription,
+//             full_description: item.fullDescription
+//         }));
 
-        console.log('Team Members:', teamMembers);
-        return teamMembers;
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
-
-// Getting Marketing team members.
-async function getMarketing() {
-    try {
-        const items = await client.items.all({
-            filter: {
-                type: 'team_member',
-                fields: {
-                    department: {
-                        eq: 'Marketing'
-                    }
-                }
-            }
-        });
-
-        const getMarketing = items.map(item => ({
-            picture: item.picture,
-            name: item.name,
-            jobTitle: item.jobTitle,
-            department: item.department,
-            short_description: item.shortDescription,
-            full_description: item.fullDescription
-        }));
-
-        console.log('Team Members:', getMarketing);
-        return getMarketing;
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
-
-// Getting Mentors department.
-async function getMentors() {
-    try {
-        const items = await client.items.all({
-            filter: {
-                type: 'team_member',
-                fields: {
-                    department: {
-                        eq: 'Mentors'
-                    }
-                }
-            }
-        });
-
-        const getMentors = items.map(item => ({
-            picture: item.picture,
-            name: item.name,
-            jobTitle: item.jobTitle,
-            department: item.department,
-            short_description: item.shortDescription,
-            full_description: item.fullDescription
-        }));
-
-        console.log('Team Members:', getMentors);
-        return getMentors;
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
-getManagers();
+//         console.log('Team Members:', teamMembers);
+//         return teamMembers;
+//     } catch (error) {
+//         console.error('Error:', error);
+//     }
+// }
 
 
 
- 
+
+
 
 
