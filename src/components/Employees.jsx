2318f5c3271ce5782cc3f24a223292c56351f5f6
Yonatan Ben Ami <yba6081@gmail.com>
2023-12-10 10:28:54 +0200

    add more

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -5,16 +5,17 @@ export default function Employees() {
 
     return (<>
         <div className='flex flex-wrap w-full justify-center gap-5'>
-            <EmployeeCard Name='Emma' Role='Web Developer' srcImg='img-3.png' />
-            <EmployeeCard Name='Emma' Role='Web Developer' srcImg='img-3.png' />
-            <EmployeeCard Name='Emma' Role='Web Developer' srcImg='img-3.png' />
-            <EmployeeCard Name='Emma' Role='Web Developer' srcImg='img-3.png' />
-            <EmployeeCard Name='Emma' Role='Web Developer' srcImg='img-3.png' />
-            <EmployeeCard Name='Emma' Role='Web Developer' srcImg='img-3.png' />
-            <EmployeeCard Name='Emma' Role='Web Developer' srcImg='img-3.png' />
-            <EmployeeCard Name='Emma' Role='Web Developer' srcImg='img-3.png' />
-            <EmployeeCard Name='Emma' Role='Web Developer' srcImg='img-3.png' />
-            <EmployeeCard Name='Emma' Role='Web Developer' srcImg='img-3.png' />
+            <EmployeeCard Name='Yonatan' Role='Frontend Developer' srcImg='yonatan-1.png' />
+            <EmployeeCard Name='Yonatan' Role='Frontend Developer' srcImg='yonatan-3.png' />
+            <EmployeeCard Name='Yonatan' Role='Frontend Developer' srcImg='yonatan-1.png' />
+            <EmployeeCard Name='Yonatan' Role='Frontend Developer' srcImg='yonatan-3.png' />
+            <EmployeeCard Name='Yonatan' Role='Frontend Developer' srcImg='yonatan-1.png' />
+            <EmployeeCard Name='Yonatan' Role='Frontend Developer' srcImg='yonatan-3.png' />
+            <EmployeeCard Name='Yonatan' Role='Frontend Developer' srcImg='yonatan-1.png' />
+            <EmployeeCard Name='Yonatan' Role='Frontend Developer' srcImg='yonatan-3.png' />
+            <EmployeeCard Name='Yonatan' Role='Frontend Developer' srcImg='yonatan-1.png' />
+            <EmployeeCard Name='Yonatan' Role='Frontend Developer' srcImg='yonatan-3.png' />
+            
         </div>
 
     </>);
