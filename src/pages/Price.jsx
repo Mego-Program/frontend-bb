a678eb890b882464916584b062902e68c2751261
ydsha <ydshamr@gmail.com>
2023-12-17 17:33:40 +0200

    cleaning

--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -48,19 +48,6 @@ export default function Price() {
     );
   }
 
-  // function Entry(props) {
-  //   return (
-  //     <div className="term">
-  //       <dt>
-  //         <span className="titel">{props.title}</span>
-  //         <span >{props.price}</span>
-  //       </dt>
-  //       <dd >{props.description}</dd>
-  //       <span className="B-button" >{props.btu}</span>
-  //     </div>
-  //   );
-  // }
-
   return (
     <>
       <HeroSection 
