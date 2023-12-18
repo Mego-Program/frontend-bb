58a0266c6b84d00d40364696efab9a72b9a46923
change the functions name <m76841@gmail.com>
2023-12-18 13:08:04 +0200

    add hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -10,7 +10,7 @@ function HeroSection(props) {
                     <div className=' w-2/3'>
                         <div className='text-left text-white text-4xl font-bold capitalize leading-10'>
                             <div className='my-2'>
-                                <span className="">{props.firstTxt}</span><br />
+                                <span className="">{props.firstTxt}</span>
                                 <span className="text-amber-400 ">{props.yellowTxt}</span><br />
                                 <span>{props.lastTxt}</span><br />
                             </div>
@@ -22,7 +22,6 @@ function HeroSection(props) {
                     </div>
                     <div className='w-1/3 flex'>
                         <img className='w-[426px] h-[424px]' src="../public/developer_with_tags.png" alt="Working Developer" title='click on me' style={{ marginTop: '210px' }} />
-                        {/* <img className='w-[64px] h-[64px]' src="../public/globle 1.png" alt="Globus Tag" title='click on me' /> */}
                     </div>
                 </div>
             </div>
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,43 +1,25 @@
-// import React from "react"
 import Benefits from "../components/Benefits"
-import { getHeroSection } from '../../cms-to-hero';
-import React, { useState, useEffect } from 'react';
+import HeroSection from "../components/HeroSection";
 
 
 export default function AboutUs() {
     
-    const [heroSection, setHeroSection] = useState({}); 
-
-    useEffect(() => {
-        async function fetchData() {
-            const heroSecs = await getHeroSection();
-            // console.log(heroSec);
-            const myHero = {}
-            heroSecs.forEach((heroSec) => {
-                myHero[heroSec.title] = heroSec.description;
-            })
-            setHeroSection(myHero);
-
-            console.log({heroSecs})
-        }
-
-        fetchData();
-    }, []);
-
-    useEffect(() => {
-        if (heroSection) {
-            console.log(heroSection); 
-        }
-    }, [heroSection]);
-    
-    
-    
     return (<>
+       
+        <HeroSection 
+                firstTxt="The art of visual "
+                yellowTxt="story telling"
+                lastTxt=""
+                smallTxt="InnovaSol is a graphic designer agency that helps brands with compelling and persuasive designs. With more tham 1 million person-hours,
+                we have worked with more than 500 brands across varios industries. Irrespective of whether you are pitching to investors, vying for clients or
+                promoting your brand in this increasungly competitive world: our creative agency ensures to deleiver a strong audience-oriented messaging."
+                buttonTxt="Learn More" />
+
         <div className="w-screen h-screen flex justify-center my-5"
             style={{ backgroundImage: "url('little-witch-folded-notes 1.png')" }}>
             <div className="w-[80%] h-[auto]">
                 <div style={{marginBottom: "20px"}}>
-                    <span className="text-white text-4xl font-bold capitalize leading-[72px]">{heroSection['AboutUs']}</span>
+                    <span className="text-white text-4xl font-bold capitalize leading-[72px]">The benefits </span>
                     <span className="text-amber-400 text-4xl font-bold capitalize leading-[72px]">for our team </span><br/>
                     <span className="text-white">The well being of each team member is really important to all of us. That is why we offer the following benefits.</span>
                 </div>
