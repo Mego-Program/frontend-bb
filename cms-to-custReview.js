86e645d2d0618add474a2f03bbd85d1765ca7737
ydsha <ydshamr@gmail.com>
2023-12-19 11:02:25 +0200

    hided api key into the variable

--- cms-to-custReview.js
+++ cms-to-custReview.js
@@ -1,6 +1,6 @@
 import { SiteClient } from 'datocms-client';
 
-const client = new SiteClient('ec1e1d0e08445b13ea2d78bf467b27');
+const client = new SiteClient(import.meta.env.VITE_TOKEN);
 
 export async function getCustomerReviews() {
     try {
