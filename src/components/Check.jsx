12172e956e058af1ecaecc6366c8ee1bbd09c967
Yonatan Ben Ami <yba6081@gmail.com>
2023-12-21 21:17:07 +0200

    Add scrolling animations

--- src/components/Check.jsx
+++ src/components/Check.jsx
@@ -1,9 +1,8 @@
 
-
 export default function Check(props) {
 
     return (<>
-        <div className="flex flex-row w-full lg:w-1/2">
+        <div className="flex flex-row w-full lg:w-1/2" data-aos="fade-left">
             <div className="flex justify-center items-center w-8 h-8 bg-slate-900 rounded-full shadow border border-indigo-950">
                 <img className="w-3.5 h-3.5" src="../public/check.png" />
             </div>
